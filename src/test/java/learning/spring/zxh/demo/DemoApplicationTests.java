b59b8bbe54cf41484b019637aafa4e12f56a0980
zxh <xinghaozhou1996@outlook.com>
2020-02-27 02:38:04 +0800

    init repo

---
+++ src/test/java/learning/spring/zxh/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.zxh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
